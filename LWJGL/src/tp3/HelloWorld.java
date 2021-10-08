77f58339141262440a3ea2810d9dbfa4ff662352
Vianney-Premier Nanda <nandanv@3il.fr>
2021-10-08 09:39:28 +0200

    TP3 fini... Bravo Vianney... Tu es trop fort

--- LWJGL/src/tp3/HelloWorld.java
+++ LWJGL/src/tp3/HelloWorld.java
@@ -13,11 +13,16 @@ import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryStack.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
+
+
 public class HelloWorld {
 
 	// The window handle
 	private long window;
-    private float rotate;
+    private float rotate = 0.0f;
+
+    int WIDTH = 1000;
+    int HEIGHT = 1000;
 
 	public void run() {
 		System.out.println("Hello LWJGL " + Version.getVersion() + "!");
@@ -47,9 +52,6 @@ public class HelloWorld {
 		glfwDefaultWindowHints(); // optional, the current window hints are already the default
 		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
 		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
-
-        int WIDTH = 800;
-        int HEIGHT = 600;
 
 		// Create the window
 		window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World!", NULL, NULL);
@@ -98,79 +100,103 @@ public class HelloWorld {
 		// creates the GLCapabilities instance and makes the OpenGL
 		// bindings available for use.
 		GL.createCapabilities();
+//init
+		// Set anti-aliazing :
+        glShadeModel(GL_SMOOTH);
 
-		// Set the clear color
+        // Set background default color :
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glClearDepth(1.0f);
+        glEnable(GL_DEPTH_TEST);
+        glDepthFunc(GL_LEQUAL);
+        glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+//Reshape
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+
+        // Set frustum :
+        float fh = 0.5f;
+        float aspect = (float) WIDTH / (float) HEIGHT;
+        float fw = fh * aspect;
+        glFrustum(-fw, fw, -fh, fh, 1.0f, 1000.0f);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+
+//display
+        // Set the clear color
 		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-        rotate = 0.0f;
+        
 
 		// Run the rendering loop until the user has attempted to close
 		// the window or has pressed the ESCAPE key.
 		while ( !glfwWindowShouldClose(window) ) {
 			
             glMatrixMode(GL_PROJECTION);
-            //glPushMatrix();
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT); // clear the framebuffer
+            //glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+            glClear(GL_COLOR_BUFFER_BIT);
+            glClear(GL_DEPTH_BUFFER_BIT);
 
         
             // Remove last render :
             glLoadIdentity();
 
             // Apply a translation to the current position :
-            glTranslatef(0.0f, 0.0f, -1.0f);
+            glTranslatef(0.0f, 0.0f, 0.5f);
 
-            glMatrixMode(GL_MODELVIEW);
+            //glMatrixMode(GL_PROJECTION);
+            //glMatrixMode(GL_MODELVIEW);
 
             // Apply a rotation to the current position :
             glRotatef(rotate, 1.0f, 0.0f, 0.0f);
             glRotatef(rotate, 0.0f, 1.0f, 0.0f);
             glRotatef(rotate, 0.0f, 0.0f, 1.0f);
 
-            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
             // Draw a Cube :
             glBegin(GL_QUADS); // Start Drawing The Cube
             
                 glColor3f(1.0f,0.0f,0.0f); //red 
-                glVertex3f(1.0f, 1.0f, -1.0f); // Top Right Of The Quad (Top)
-                glVertex3f( -1.0f, 1.0f, -1.0f); // Top Left Of The Quad (Top)
-                glVertex3f( -1.0f, 1.0f, 1.0f ); // Bottom Left Of The Quad (Top)
-                glVertex3f( 1.0f, 1.0f, 1.0f ); // Bottom Right Of The Quad (Top)
+                glVertex3f(0.5f, 0.5f, -0.5f); // Top Right Of The Quad (Top)
+                glVertex3f( -0.5f, 0.5f, -0.5f); // Top Left Of The Quad (Top)
+                glVertex3f( -0.5f, 0.5f, 0.5f ); // Bottom Left Of The Quad (Top)
+                glVertex3f( 0.5f, 0.5f, 0.5f ); // Bottom Right Of The Quad (Top)
                     
                 glColor3f( 0.0f,1.0f,0.0f ); //green 
-                glVertex3f( 1.0f, -1.0f, 1.0f ); // Top Right Of The Quad
-                glVertex3f( -1.0f, -1.0f, 1.0f ); // Top Left Of The Quad
-                glVertex3f( -1.0f, -1.0f, -1.0f ); // Bottom Left Of The Quad
-                glVertex3f( 1.0f, -1.0f, -1.0f ); // Bottom Right Of The Quad 
+                glVertex3f( 0.5f, -0.5f, 0.5f ); // Top Right Of The Quad
+                glVertex3f( -0.5f, -0.5f, 0.5f ); // Top Left Of The Quad
+                glVertex3f( -0.5f, -0.5f, -0.5f ); // Bottom Left Of The Quad
+                glVertex3f( 0.5f, -0.5f, -0.5f ); // Bottom Right Of The Quad 
 
                 glColor3f( 0.0f,0.0f,1.0f ); //blue 
-                glVertex3f( 1.0f, 1.0f, 1.0f ); // Top Right Of The Quad (Front)
-                glVertex3f( -1.0f, 1.0f, 1.0f ); // Top Left Of The Quad (Front)
-                glVertex3f( -1.0f, -1.0f, 1.0f ); // Bottom Left Of The Quad
-                glVertex3f( 1.0f, -1.0f, 1.0f ); // Bottom Right Of The Quad 
+                glVertex3f( 0.5f, 0.5f, 0.5f ); // Top Right Of The Quad (Front)
+                glVertex3f( -0.5f, 0.5f, 0.5f ); // Top Left Of The Quad (Front)
+                glVertex3f( -0.5f, -0.5f, 0.5f ); // Bottom Left Of The Quad
+                glVertex3f( 0.5f, -0.5f, 0.5f ); // Bottom Right Of The Quad 
 
                 glColor3f( 1.0f,1.0f,0.0f ); //yellow
-                glVertex3f( 1.0f, -1.0f, -1.0f ); // Bottom Left Of The Quad
-                glVertex3f( -1.0f, -1.0f, -1.0f ); // Bottom Right Of The Quad
-                glVertex3f( -1.0f, 1.0f, -1.0f ); // Top Right Of The Quad (Back)
-                glVertex3f( 1.0f, 1.0f, -1.0f ); // Top Left Of The Quad (Back)
+                glVertex3f( 0.5f, -0.5f, -0.5f ); // Bottom Left Of The Quad
+                glVertex3f( -0.5f, -0.5f, -0.5f ); // Bottom Right Of The Quad
+                glVertex3f( -0.5f, 0.5f, -0.5f ); // Top Right Of The Quad (Back)
+                glVertex3f( 0.5f, 0.5f, -0.5f ); // Top Left Of The Quad (Back)
 
                 glColor3f( 1.0f,0.0f,1.0f ); //purple
-                glVertex3f( -1.0f, 1.0f, 1.0f ); // Top Right Of The Quad (Left)
-                glVertex3f( -1.0f, 1.0f, -1.0f ); // Top Left Of The Quad (Left)
-                glVertex3f( -1.0f, -1.0f, -1.0f ); // Bottom Left Of The Quad
-                glVertex3f( -1.0f, -1.0f, 1.0f ); // Bottom Right Of The Quad 
+                glVertex3f( -0.5f, 0.5f, 0.5f ); // Top Right Of The Quad (Left)
+                glVertex3f( -0.5f, 0.5f, -0.5f ); // Top Left Of The Quad (Left)
+                glVertex3f( -0.5f, -0.5f, -0.5f ); // Bottom Left Of The Quad
+                glVertex3f( -0.5f, -0.5f, 0.5f ); // Bottom Right Of The Quad 
 
                 glColor3f( 0.0f,1.0f, 1.0f ); //sky blue
-                glVertex3f( 1.0f, 1.0f, -1.0f ); // Top Right Of The Quad (Right)
-                glVertex3f( 1.0f, 1.0f, 1.0f ); // Top Left Of The Quad
-                glVertex3f( 1.0f, -1.0f, 1.0f ); // Bottom Left Of The Quad
-                glVertex3f( 1.0f, -1.0f, -1.0f ); // Bottom Right Of The Quad
+                glVertex3f( 0.5f, 0.5f, -0.5f ); // Top Right Of The Quad (Right)
+                glVertex3f( 0.5f, 0.5f, 0.5f ); // Top Left Of The Quad
+                glVertex3f( 0.5f, -0.5f, 0.5f ); // Bottom Left Of The Quad
+                glVertex3f( 0.5f, -0.5f, -0.5f ); // Bottom Right Of The Quad
             
             glEnd(); // Done Drawing The Quad
+            //glFlush();
             //glPopMatrix();
 
             // Increase rotation :
-            rotate += 0.2f;
+            rotate += 0.5f;
 
 			glfwSwapBuffers(window); // swap the color buffers
 
